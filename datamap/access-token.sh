e00a095fbc5952bb1d1031ce1eaa753e43cde2d7
kidong-park <kidong@datanuri.net>
2019-09-17 13:32:44 +0900

    delete datamap

--- datamap/access-token.sh
+++
@@ -1,1 +0,0 @@
-curl -k -X POST -d "client_id=platform&client_secret=a329cb95-8141-4a38-93c9-b81dc2a91268&grant_type=password&username=$1&password=so8087" https://keycloak-web-58-181-37-175.nip.io:7002/auth/realms/master/protocol/openid-connect/token | json_pp
--- datamap/src/utils/locales.js
+++
@@ -1,8 +0,0 @@
-/**
- *  Copyright (c) Electronics and Telecommunications Research Institute. All rights reserved.
- *  See License.txt in the project root for license information.
- */
-
-const i18next = require('i18next')
-
-module.exports = i18next
